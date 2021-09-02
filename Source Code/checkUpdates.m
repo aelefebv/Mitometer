9d5788ed7ca9908e9c136e469e2f1b8903d1cda0
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2021-09-02 08:55:11 -0700

    GUI now points to correct version.txt

--- Source Code/checkUpdates.m
+++ Source Code/checkUpdates.m
@@ -2,7 +2,7 @@ function updates = checkUpdates()
 
 thisVersion = textread('version.txt');
 
-[newVersion,status] = urlread('https://raw.githubusercontent.com/aelefebv/Mitometer/Revisions/Source%20Code/version.txt');
+[newVersion,status] = urlread('https://raw.githubusercontent.com/aelefebv/Mitometer/main/Source%20Code/version.txt');
 
 if status~=0 && thisVersion<str2double(newVersion)
     updates = 1;
